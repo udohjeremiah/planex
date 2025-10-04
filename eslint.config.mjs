a2f4e467f3b136903975bdd39dfa786eefcf6f0b
Udoh Jeremiah <udohjeremiah@icloud.com>
2025-10-04 12:19:37 +0100

    Add ESLint for linting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,68 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+import { fixupConfigRules } from "@eslint/compat";
 import { FlatCompat } from "@eslint/eslintrc";
+import js from "@eslint/js";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
+import unicorn from "eslint-plugin-unicorn";
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __dirname = path.dirname(__filename);
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
 });
 
+/**
+ * @see https://eslint.org/docs/latest/use/configure/configuration-files
+ * @type {import("eslint").Linter.Config[]}
+ */
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...fixupConfigRules(
+    compat.extends(
+      "next/core-web-vitals",
+      "next/typescript",
+      "plugin:import/recommended",
+      "plugin:prettier/recommended",
+    ),
+  ),
+  unicorn.configs.recommended,
+  {
+    plugins: {
+      "simple-import-sort": simpleImportSort,
+    },
+    rules: {
+      "simple-import-sort/exports": "error",
+      "simple-import-sort/imports": "error",
+      "unicorn/no-array-callback-reference": "off",
+      "unicorn/no-array-for-each": "off",
+      "unicorn/no-array-reduce": "off",
+      "unicorn/no-empty-file": "off",
+      "unicorn/prevent-abbreviations": [
+        "error",
+        {
+          allowList: {
+            env: true,
+            e2e: true,
+          },
+          replacements: {
+            props: false,
+            ref: false,
+            params: false,
+          },
+        },
+      ],
+    },
+  },
+  {
+    files: ["**/*.js"],
+    rules: {
+      "unicorn/prefer-module": "off",
+    },
+  },
   {
     ignores: [
       "node_modules/**",
